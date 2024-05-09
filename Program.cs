dc64c2e443fc55ea69ab6f82ed55f35e03e6bd6a
quanggiap2004 <nguyenquanggiap642004@gmail.com>
2024-05-09 11:24:55 +0700

    upload 1st time

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HappyNewYear;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Giáp đẹp trai pro vjp vkl");
+	Console.WriteLine("\nPress any key to exit...");
+	Console.ReadLine(); //wait for someone to enter something
+    }
+}
